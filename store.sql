60f1176751aa8356fd15b1bcda047b5734a79e60
Adham23P <apopal23@gwu.edu>
2023-04-09 15:12:54 -0400

    Initial parsing example with relations defined

--- store.sql
+++ store.sql
@@ -1,20 +1,45 @@
 DROP TABLE IF EXISTS Users;
-CREATE TABLE users (
-  user_id        varchar(20) not null,
-  user_login     varchar(70) not null,
-  displaylogin   varchar(70) not null,
-  gravatar_id    varchar(50) not null,
-  user_url       varchar(150) not null,
-  avatar_url     varchar(150) not null,
+CREATE TABLE Users (
+  user_id        varchar(20) not NULL,
+  user_login     varchar(70) not NULL,
+  display_login   varchar(70) not NULL,
+  gravatar_id    varchar(50),
+  user_url       varchar(150) not NULL,
+  avatar_url     varchar(150) not NULL,
   PRIMARY KEY (user_id)
 );
 
 DROP TABLE IF EXISTS Repo;
-CREATE TABLE repo (
+CREATE TABLE Repo (
   repo_id     varchar(20) not NULL,
   repo_name   varchar(50) not NULL,
   repo_url    varhchar(150) not NULL,
   PRIMARY KEY (repo_id)
+);
+
+DROP TABLE IF EXISTS Org;
+CREATE TABLE Org (
+  org_id      varchar(20) not NULL,
+  org_login   varchar(70) not NULL,
+  gravatar_id varchar(50),
+  org_url     varchar(150),
+  avatar_url  varchar(150),
+  PRIMARY KEY (org_id)
+);
+
+DROP TABLE IF EXISTS EventRelation;
+CREATE TABLE EventRelation (
+  user_id     varchar(20) not NULL,
+  repo_id     varchar(20) not NULL,
+  event_type  varchar(50) not NULL, 
+  event_id    varchar(20) not NULL,
+  org_id      varchar(20),
+  public_evt  BOOLEAN,
+  created_at  DATETIME,
+  PRIMARY KEY (event_id),
+  FOREIGN KEY (repo_id) REFERENCES Repo(repo_id),
+  FOREIGN KEY (user_id) REFERENCES Users(user_id),
+  FOREIGN KEY (org_id) REFERENCES Org(org_id)
 );
 
 DROP TABLE IF EXISTS CreateEvent;
@@ -25,11 +50,16 @@ CREATE TABLE CreateEvent (
   master_branch   varchar(15) not NULL,
   create_descrip  varchar(200),
   pusher_type     varchar(20),
-  public_evt      BOOLEAN not NULL,
-  created_at      DATETIME,
-  repo_id         varchar(20) not NULL,
-  user_id_execute varchar(20) not NULL,
   PRIMARY KEY (create_id),
-  Foreign Key (repo_id) REFERENCES repo(repo_id),
-  Foreign Key (user_id_execute) REFERENCES users(user_id)
-)
+  FOREIGN KEY (create_id) REFERENCES EventRelation(event_id)
+);
+
+DROP TABLE IF EXISTS DeleteEvent;
+CREATE TABLE DeleteEvent (
+  delete_id   varchar(20) not NULL,
+  delete_ref  varchar(150) not NULL,
+  ref_type    varchar(20) not NULL,
+  pusher_type varchar(20) not NULL,
+  PRIMARY KEY (delete_id),
+  FOREIGN KEY (delete_id) REFERENCES EventRelation(event_id)
+);
